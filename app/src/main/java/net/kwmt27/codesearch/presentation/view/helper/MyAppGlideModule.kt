61e80605a84d67c9e95d646ef8735138e8bafe0f
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-10-25 10:00:57 +0900

    add漏れ

---
+++ app/src/main/java/net/kwmt27/codesearch/presentation/view/helper/MyAppGlideModule.kt
@@ -0,0 +1,25 @@
+package net.kwmt27.codesearch.presentation.view.helper
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Glide v4 uses an annotation processor to generate an API that allows applications to access all options
+ * in RequestBuilder, RequestOptions and any included integration libraries in a single fluent API.
+ *
+ * http://bumptech.github.io/glide/doc/generatedapi.html
+ *
+ * たとえば、centerCropはGlideにはないが、RequestBuilderが持っているcenterCropメソッドを次のようにメソッドチェーンで書くことができるようになる。
+ *
+ * ```
+ * GlideApp
+ * .with(imageView.context)
+ * .load(imageUrl)
+ * .centerCrop()
+ * .placeholder(placeholderResId)
+ * .into(imageView)
+ * ```
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule(){
+}
