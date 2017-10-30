0429ed7ccd7fad9f7f5cec44ebf33482aee63b31
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-10-30 14:17:30 +0900

    Fix typo

--- app/src/main/java/net/kwmt27/codesearch/infrastructure/entity/mapper/EventEntityModelMapper.kt
+++ app/src/main/java/net/kwmt27/codesearch/infrastructure/entity/mapper/EventEntityModelMapper.kt
@@ -1,13 +1,13 @@
 package net.kwmt27.codesearch.infrastructure.entity.mapper
 
-import net.kwmt27.codesearch.infrastructure.entity.EventEntity
 import net.kwmt27.codesearch.domain.model.EventModel
 import net.kwmt27.codesearch.domain.model.valueobject.GithubUser
+import net.kwmt27.codesearch.infrastructure.entity.EventEntity
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
- * [EventEntity](net.kwmt27.codesearch.data.entity.EventEntity)から[EvnetModel](net.kwmt27.codesearch.domain.model.EvnetModel)に変換する
+ * [EventEntity](net.kwmt27.codesearch.data.entity.EventEntity)から[EventModel](net.kwmt27.codesearch.domain.model.EventModel)に変換する
  */
 @Singleton
 class EventEntityModelMapper @Inject constructor() {
