923ff70f942f967f19e92177982b7ce6ff8dc6e7
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-10-30 11:25:47 +0900

    改行

--- app/src/main/java/net/kwmt27/codesearch/infrastructure/repository/EventsDataRepository.kt
+++ app/src/main/java/net/kwmt27/codesearch/infrastructure/repository/EventsDataRepository.kt
@@ -1,16 +1,19 @@
 package net.kwmt27.codesearch.infrastructure.repository
 
 import io.reactivex.Single
-import net.kwmt27.codesearch.infrastructure.entity.mapper.EventEntityModelMapper
-import net.kwmt27.codesearch.infrastructure.repository.datesource.EventsDataSourceManager
 import net.kwmt27.codesearch.domain.model.EventModel
 import net.kwmt27.codesearch.domain.repository.EventsRepository
+import net.kwmt27.codesearch.infrastructure.entity.mapper.EventEntityModelMapper
+import net.kwmt27.codesearch.infrastructure.repository.datesource.EventsDataSourceManager
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class EventsDataRepository
-@Inject constructor(private val eventsDataSourceManager: EventsDataSourceManager, private val eventEntityModelMapper: EventEntityModelMapper) : EventsRepository {
+@Inject constructor(
+        private val eventsDataSourceManager: EventsDataSourceManager,
+        private val eventEntityModelMapper: EventEntityModelMapper
+) : EventsRepository {
 
 
 
