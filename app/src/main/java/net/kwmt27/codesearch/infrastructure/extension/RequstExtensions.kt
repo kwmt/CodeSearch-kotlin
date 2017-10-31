82f8c7a4efe20e84d355b64ab453b0c98f5d6503
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-01 02:05:53 +0900

    add漏れ

--- app/src/main/java/net/kwmt27/codesearch/infrastructure/extension/RequstExtensions.kt
+++ app/src/main/java/net/kwmt27/codesearch/infrastructure/extension/RequstExtensions.kt
@@ -1,4 +1,4 @@
-package net.kwmt27.codesearch.infrastructure.util
+package net.kwmt27.codesearch.infrastructure.extension
 
 import okhttp3.Request
 import okhttp3.RequestBody
@@ -6,43 +6,35 @@ import okio.Buffer
 import timber.log.Timber
 import java.io.IOException
 
+
 /**
- * API専用ユーティリティ
+ * [Request] からcurl形式でログ出力する。
+ * @param request [Request]
  */
-object ApiUtil {
+inline fun Request.printCurlString() {
+    val bodyString = body()?.toStringFromRequestBody() ?: ""
+    val contentType = body()?.contentType()?.toString() ?: ""
 
-    /**
-     * [Request] からcurl形式でログ出力する。
-     * @param request [Request]
-     */
-    fun printCurlString(request: Request) {
-        val url = request.url().toString()
-        val method = request.method()
+    val headers = headers()
+    val result = StringBuilder()
 
-        val body = request.body()
-        val bodyString = body?.let { toStringFromRequestBody(body) } ?: ""
-        val contentType = body?.contentType()?.toString() ?: ""
-
-        val headers = request.headers()
-        val result = StringBuilder()
-
-        if(headers.size() > 0) {
-            for (i in 0..headers.size()) {
-                result.append(" -H '" + headers.name(i)).append(": ").append(headers.value(i) + "'")
-            }
+    if (headers.size() > 0) {
+        for (i in 0..headers.size()) {
+            result.append(" -H '" + headers.name(i)).append(": ").append(headers.value(i) + "'")
         }
-        val headersString = result.toString()
-        Timber.d("curl  -X $method \\\n -d '$bodyString' \\\n -H '$contentType'$headersString \\\n '$url'\n")
     }
-
-    private fun toStringFromRequestBody(body: RequestBody): String {
-        try {
-            val buffer = Buffer()
-            body.writeTo(buffer)
-            return buffer.readUtf8()
-        } catch (e: IOException) {
-            return ""
-        }
-
-    }
+    Timber.d("curl  -X ${method()} \\\n -d '$bodyString' \\\n -H '$contentType'$result \\\n '${url()}'\n")
 }
+
+
+inline fun RequestBody.toStringFromRequestBody(): String {
+    try {
+        val buffer = Buffer()
+        writeTo(buffer)
+        return buffer.readUtf8()
+    } catch (e: IOException) {
+        return ""
+    }
+
+}
+
