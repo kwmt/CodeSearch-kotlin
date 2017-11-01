77cabd6c1642b7496ff0547af559dc1115eb8328
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-11-01 10:59:16 +0900

    .Navigator is bound multiple times

--- app/src/main/java/net/kwmt27/codesearch/presentation/eventlist/di/EventsFragmentModule.kt
+++ app/src/main/java/net/kwmt27/codesearch/presentation/eventlist/di/EventsFragmentModule.kt
@@ -1,20 +1,15 @@
 package net.kwmt27.codesearch.presentation.eventlist.di
 
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 import dagger.Module
-import dagger.Provides
 import net.kwmt27.codesearch.application.di.FragmentScope
-import net.kwmt27.codesearch.presentation.eventlist.EventsFragment
-import net.kwmt27.codesearch.presentation.eventlist.Navigator
 
 @FragmentScope
 @Module
 class EventsFragmentModule {
-    @Provides
-    fun provideFragment(fragmentManager: FragmentManager): Fragment = fragmentManager.findFragmentByTag(EventsFragment.TAG)
-
-    @Provides
-    fun provideNavigator(fragment: Fragment): Navigator = Navigator(fragment)
+//    @Provides
+//    fun provideFragment(fragmentManager: FragmentManager): Fragment = fragmentManager.findFragmentByTag(EventsFragment.TAG)
+//
+//    @Provides
+//    fun provideNavigator(fragment: Fragment): Navigator = Navigator(fragment)
 
 }
